564a7484adfb7027b40a50cf2fefc4e8ff7ec603
yuvatimankar <yuvatimankar06@gmail.com>
2021-05-30 21:56:09 +0530

    changes

--- server.js
+++ server.js
@@ -9,12 +9,16 @@ const signin = require('./controllers/signin');
 const profile= require('./controllers/profile');
 const image= require('./controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+
 const db = knex({
   // Enter your own database information here based on what you created
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl:true
+    ssl:{
+      rejectUnauthorized:false
+    }
   }
 });
 
